8c0ed2681baebf6f5afa66a2b8fef3d1a32f62e8
2HamidSohail <151230230+2HamidSohail@users.noreply.github.com>
2024-03-30 16:27:19 +0500

    Initial commit

---
+++ src/app/name/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+const Name =()=>{
+    return (
+        <div>
+            <h1>Name Page</h1>
+            <Link href="/"> Go to Homepage</Link>
+        </div>
+    )
+}
+export default Name;
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function Home() {
+  return (
+    <main>
+    <h1>Basic Routing | Make New Page </h1>
+    <br />
+    <Link href="/name"> Go to Name Page</Link>
+    </main>
+  )
+}
