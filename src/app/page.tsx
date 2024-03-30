318d9c62cb2db19a43f7f61fe45873bffb149e15
2HamidSohail <151230230+2HamidSohail@users.noreply.github.com>
2024-03-30 16:29:37 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main>
+      <h1>HomePage</h1>
     <h1>Basic Routing | Make New Page </h1>
     <br />
     <Link href="/name"> Go to Name Page</Link>
